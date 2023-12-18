b82183823877052cd759d3c3bbf144d8b09f431c
Angad Singh <mail@angad.dev>
2023-12-18 06:45:23 +0530

    chore: setup migrations

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,11 +1,11 @@
+import { sql } from "drizzle-orm";
 import { pgTable, serial, date, varchar } from "drizzle-orm/pg-core";
-import db from ".";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 200 }),
   email: varchar("email", { length: 200 }),
-  createdat: date("createdat"),
+  createdat: date("createdat").default(sql`current_timestamp`),
 });
 export type User = typeof users.$inferSelect; // return type when queried
 export type NewUser = typeof users.$inferInsert; // insert type
